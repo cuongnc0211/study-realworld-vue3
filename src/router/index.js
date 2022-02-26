16fc6fd0e8e3117ffe10a7635c7b3a13fc48fc55
cuongnc0211 <cuongnguyenfu@gmail.com>
2022-02-26 17:11:34 +0700

    init

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '@/views/EventList.vue'
+import EventDetail from '@/views/EventDetail.vue'
+import About from '@/views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventList
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetail',
+    props: true,
+    component: EventDetail
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
